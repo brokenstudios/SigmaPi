a9572f6cce0e7a9e1484686e81f46f653a3b7592
Loïc Gillioz <loic.gillioz@outlook.com>
2016-04-14 16:32:34 +0200

    next step make layout class and implement 3 instances in Unit + one more later for the arms

--- src/hevs/fragil/patapon/others/Param.java
+++ src/hevs/fragil/patapon/others/Param.java
@@ -2,7 +2,7 @@ package hevs.fragil.patapon.others;
 
 import com.badlogic.gdx.graphics.Color;
 
-public class Param {
+public abstract class Param {
 	//Game parameters only
 	//TODO all of customizable elements should be here !
 	
@@ -17,6 +17,12 @@ public class Param {
 	//Style 
 	public static final int FLOOR_DEPTH = 30;
 	public static final int FRAME_DEGRADE_STEPS = 10;
+	//Break the color in case of emergency #wedontneedthatbutitwassocolorfulwhenyoutouchme
+	public static final Color Type1 = getFloatColor(243, 146, 0);
+	public static final Color Type2 = getFloatColor(227, 6, 19);
+	public static final Color Type3 = getFloatColor(230, 0, 126);
+	public static final Color Type4 = getFloatColor(0, 159, 227);
+	public static final Color Type5 = getFloatColor(58, 170, 53);
 	
 	//Timer periods
 	public static final int MUSIC_BAR = 500;
@@ -36,5 +42,11 @@ public class Param {
 	
 	//Colors
 	public static Color BACKGROUND = new Color(1.000f, 0.871f, 0.678f, 0.1f);
-	
+	private static Color getFloatColor(int r, int g, int b){
+		Color temp = new Color();
+		temp.r = (float)(r/255.0);
+		temp.g = (float)(g/255.0);
+		temp.b = (float)(b/255.0);
+		return temp;
+	}
 }
---
+++ src/hevs/fragil/patapon/units/Breed.java
@@ -0,0 +1,5 @@
+package hevs.fragil.patapon.units;
+
+public enum Breed {
+	Breed1,Breed2,Breed3,Breed4,Breed5;
+}
