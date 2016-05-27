b1f289dbebccace37c8ed62dff86ce9e274af53d
Loic Fracheboud <loic.fracheboud@gmail.com>
2016-05-27 13:51:39 +0200

    Forrest update

--- src/hevs/fragil/patapon/drawables/Tree.java
+++ src/hevs/fragil/patapon/drawables/Tree.java
@@ -9,7 +9,7 @@ import ch.hevs.gdx2d.lib.interfaces.DrawableObject;
 import hevs.fragil.patapon.mechanics.Param;
 
 public class Tree implements DrawableObject {
-	
+
 	private Random r;
 	private long seed;
 	private int time = 0;
@@ -27,38 +27,41 @@ public class Tree implements DrawableObject {
 		this.size = size;
 		this.width = width;
 	}
-	
-/**
+
+	/**
 	 * Draws a line {@code length} long
-	 * @param 	length 		length of the branch
-	 * @author 	loicg
+	 * 
+	 * @param length
+	 *            length of the branch
+	 * @author loicg
 	 */
-	private void drawDoubleLine(double length, double width){
-		//draw extern line
+	private void drawDoubleLine(double length, double width) {
+		// draw extern line
 		t.turn(-90);
 		t.penUp();
 		t.forward(width);
 		t.turn(90);
 		t.penDown();
 		t.forward(length);
-		double oldAngle = t.getTurtleAngle(); 
+		double oldAngle = t.getTurtleAngle();
 		t.penUp();
 		t.turn(90);
 		t.forward(width);
 		Point<Float> oldPos = t.getPosition();
 		t.forward(width);
-		
-		//draw intern line
+
+		// draw intern line
 		t.turn(90);
 		t.penDown();
 		t.forward(length);
-		//return to old pos
+		// return to old pos
 		t.jump(oldPos.x, oldPos.y);
 		t.setAngle(oldAngle);
 		t.penUp();
 	}
-	private void drawLine(double length, double width){
-		if(r.nextDouble() > 0.5)
+
+	private void drawLine(double length, double width) {
+		if (r.nextDouble() > 0.5)
 			drawDoubleLine(length, width);
 		else {
 			t.penDown();
@@ -66,127 +69,148 @@ public class Tree implements DrawableObject {
 		}
 		t.penUp();
 	}
-	private void drawHexaBranch (double length, double width){
-		//hexa node
-		if(r.nextDouble() > 0.2){
-			drawLine(length/3, width);
-			drawHexagon(length/6);
-			drawLine(length/3, width);
-		} 
-		else{
-			drawLine(length/3, width);
+
+	private void drawHexaBranch(double length, double width) {
+		// hexa node
+		if (r.nextDouble() > 0.2) {
+			drawLine(length / 3, width);
+			drawHexagon(length / 6);
+			drawLine(length / 3, width);
+		} else {
+			drawLine(length / 3, width);
 		}
 	}
+
 	/**
-	 * Verifies {@code value} is between {@code min} and {@code max}, then change it if out of range
-	 * @param value	the value to limit
-	 * @param min	minimum value for {@code value} 
-	 * @param max	maximum value for {@code value} 
+	 * Verifies {@code value} is between {@code min} and {@code max}, then
+	 * change it if out of range
+	 * 
+	 * @param value
+	 *            the value to limit
+	 * @param min
+	 *            minimum value for {@code value}
+	 * @param max
+	 *            maximum value for {@code value}
 	 * @author loicg
 	 * @return
 	 */
 	private double ensureRange(double value, double min, double max) {
-		   return Math.min(Math.max(value, min), max);
-		}
-	private void drawHexaPart(double length){
-		//draw extern line
+		return Math.min(Math.max(value, min), max);
+	}
+
+	private void drawHexaPart(double length) {
+		// draw extern line
 		t.forward(length);
 		Point<Float> oldPos = t.getPosition();
-		double oldAngle = t.getTurtleAngle(); 
-		
+		double oldAngle = t.getTurtleAngle();
+
 		t.penUp();
 		t.turn(120);
-		t.forward(length/3);
-		
-		//draw intern line
+		t.forward(length / 3);
+
+		// draw intern line
 		t.turn(60);
 		t.penDown();
-		t.forward(length*2/3);
-		//return to old pos
+		t.forward(length * 2 / 3);
+		// return to old pos
 		t.jump(oldPos.x, oldPos.y);
 		t.setAngle(oldAngle);
 	}
-	private void drawSimpleHexagon(double length){
+
+	private void drawSimpleHexagon(double length) {
 		t.penDown();
 		t.turn(-60);
-		for(int i = 0; i<6; i++){			
-			t.forward(length);;
+		for (int i = 0; i < 6; i++) {
+			t.forward(length);
+			;
 			t.turn(60);
 		}
 		t.penUp();
 		t.turn(60);
-		t.forward(2*length);
+		t.forward(2 * length);
 	}
-	private void drawDoubleHexagon(double length){
+
+	private void drawDoubleHexagon(double length) {
 		t.penDown();
 		t.turn(-60);
-		for(int i = 0; i<6; i++){			
-			if(i%2==0)
+		for (int i = 0; i < 6; i++) {
+			if (i % 2 == 0)
 				drawHexaPart(length);
-			else 
-				t.forward(length);;
+			else
+				t.forward(length);
+			;
 			t.turn(60);
 		}
 		t.penUp();
 		t.turn(60);
-		t.forward(2*length);
+		t.forward(2 * length);
 	}
-	private void drawHexagon(double length){
-		if(r.nextDouble() > 0.5)
+
+	private void drawHexagon(double length) {
+		if (r.nextDouble() > 0.5)
 			drawDoubleHexagon(length);
-		else 
+		else
 			drawSimpleHexagon(length);
 	}
+
 	/**
 	 * Draws a random tree
-	 * @param n				complexity of recursive function (n sub-calls)
-	 * @param length 		length of the first branch
-	 * @param width			width of the first branch
-	 * @param leavesColor	average color of the leaves
+	 * 
+	 * @param n
+	 *            complexity of recursive function (n sub-calls)
+	 * @param length
+	 *            length of the first branch
+	 * @param width
+	 *            width of the first branch
+	 * @param leavesColor
+	 *            average color of the leaves
 	 * @author loicg
 	 */
-	private void drawHexaTree (int n, double length, int width){
-		//basis width
+	private void drawHexaTree(int n, double length, int width) {
+		// basis width
 		t.setWidth(width);
-		int factor = (complexity-n)*10;
-		if(n > 1){
-			//draw basis 
+		int factor = (complexity - n) * 10;
+		if (n > 1) {
+			// draw basis
 			drawHexaBranch(length, width);
-			
-			//save Y embranchement
-			Point<Float> oldPos = t.getPosition();
-			double oldAngle = t.getTurtleAngle(); 
-			
-			//next left tree (with new random factors)
-			t.setAngle(oldAngle + 60 + (factor * Math.sin( (time*2*Math.PI)/360+(r.nextDouble()*10))));
-			double newLength = length * 2/3;
-			drawHexaTree(n-1, newLength, (int) ensureRange(width*2/3, 1, 10));
-			
-			//return to old Y embranchement
-			t.jump(oldPos.x, oldPos.y);
-			
-			//next right tree (with new random factors)
-			t.setAngle(oldAngle - 60 + (factor * Math.sin((time*2*Math.PI)/360+(r.nextDouble()*10))));
-			drawHexaTree(n-1, newLength, (int) ensureRange(width*2/3, 1, 10));
 
-		}
-		else 
+			// save Y embranchement
+			Point<Float> oldPos = t.getPosition();
+			double oldAngle = t.getTurtleAngle();
+
+			// next left tree (with new random factors)
+			t.setAngle(oldAngle + 60 + (factor * Math.sin((time * 2 * Math.PI) / 360 + (r.nextDouble() * 10))));
+			double newLength = length * 2 / 3;
+			drawHexaTree(n - 1, newLength, (int) ensureRange(width * 2 / 3, 1, 10));
+
+			// return to old Y embranchement
+			t.jump(oldPos.x, oldPos.y);
+
+			// next right tree (with new random factors)
+			t.setAngle(oldAngle - 60 + (factor * Math.sin((time * 2 * Math.PI) / 360 + (r.nextDouble() * 10))));
+			drawHexaTree(n - 1, newLength, (int) ensureRange(width * 2 / 3, 1, 10));
+
+		} else
 			drawHexaBranch(length, width);
 	}
 
 	@Override
 	public void draw(GdxGraphics g) {
-		if(t != null){
-			//for oscillation
-			time += 4;
-			t.jump(location.x, location.y);
-			t.setAngle(90);
-			//reset values (get the same tree as last one)
-			r.setSeed(seed);
-			drawHexaTree(complexity, size, width);
-		}
-		else 
-			t = new Turtle(g,Param.WIN_WIDTH,Param.WIN_HEIGHT);
+		float camPosX = g.getCamera().position.x;
 		
+		//Check if the trees are on screen
+		if(location.x < camPosX + Param.WIN_WIDTH/2 && location.x > camPosX - Param.WIN_WIDTH/2){
+			if (t != null) {
+				// for oscillation
+				time += 4;
+				t.jump(location.x, location.y);
+				t.setAngle(90);
+				// reset values (get the same tree as last one)
+				r.setSeed(seed);
+				drawHexaTree(complexity, size, width);
+			} else
+				t = new Turtle(g, Param.WIN_WIDTH, Param.WIN_HEIGHT);
+	
+		}
 	}
 }
--- src/hevs/fragil/patapon/mechanics/Decor.java
+++ src/hevs/fragil/patapon/mechanics/Decor.java
@@ -16,89 +16,101 @@ public class Decor {
 	private int height;
 	private Color background;
 	private Vector3 cameraPos;
-	//Contain the objects to draw (trees, etc..)
+	// Contain the objects to draw (trees, etc..)
 	private Vector<DrawableObject> toDraw = new Vector<DrawableObject>();
-	
+
 	// Dynamic floor width
-	public Decor(int w, int h, Color b){
+	public Decor(int w, int h, Color b) {
 		this.width = w;
 		this.setBackground(b);
-		processTree();
+		// processTree();
+		Point<Float> origin = new Point<Float>(0f, (float) Param.FLOOR_DEPTH);
+		processForest(8, origin, 5, 200f, 5);
 	}
-	
-	public Vector3 cameraProcess(Company c1, Company c2){
-		//Camera always stick on the floor
+
+	public Vector3 cameraProcess(Company c1, Company c2) {
+		// Camera always stick on the floor
 		cameraPos.y = 0;
-		
-		//Get the companies positions
-		int x1 = c1.globalPosition;
-		int x2 = c2.globalPosition;
-		
-		//TODO verify if both companies are close enough to be shown
-		if(Math.abs(x2-x1) < Param.WIN_WIDTH){
-			//TODO caracteriser le zoom mathematiquement, c'est a dire f(w,h)
+
+		// Get the companies positions
+		float x1 = c1.getPosition();
+		float x2 = c2.getPosition();
+
+		// TODO verify if both companies are close enough to be shown
+		if (Math.abs(x2 - x1) < Param.WIN_WIDTH) {
+			// TODO caracteriser le zoom mathematiquement, c'est a dire f(w,h)
 		}
-		
-		if(x1 >= 0 && x1 < Param.WIN_WIDTH && x1 >= 0 && x1 < Param.WIN_WIDTH){
-			if(x1 < x2){
-				cameraPos.x = x2-x1 + Param.CAMERAOFFSET;
+
+		if (x1 >= 0 && x1 < Param.WIN_WIDTH && x1 >= 0 && x1 < Param.WIN_WIDTH) {
+			if (x1 < x2) {
+				cameraPos.x = x2 - x1 + Param.CAMERAOFFSET;
+			} else {
+				cameraPos.x = x1 - x2 + Param.CAMERAOFFSET;
 			}
-			else{
-				cameraPos.x = x1-x2 + Param.CAMERAOFFSET;
-			}
-		}
-		else{
-			//Input invalid!
+		} else {
+			// Input invalid!
 			cameraPos.x = Param.CAMERAOFFSET;
 			cameraPos.z = 0;
 		}
-		
-		//TODO process camera position depending of objects position AND POV
-		//TODO ensure that zoom is in limits (dynamic, process with height to not
-		//display more than the map (or with the width if width<height)
-		
+
+		// TODO process camera position depending of objects position AND POV
+		// TODO ensure that zoom is in limits (dynamic, process with height to
+		// not
+		// display more than the map (or with the width if width<height)
+
 		return cameraPos;
 	}
-	public void processTree(){
-		Point<Float> pos = new Point<Float>(500f,(float)Param.FLOOR_DEPTH);
-		toDraw.addElement(new Tree(pos,3,200f,5));
+
+	public void processTree() {
+		Point<Float> pos = new Point<Float>(500f, (float) Param.FLOOR_DEPTH);
+		toDraw.addElement(new Tree(pos, 3, 200f, 5));
 	}
-	
+
+	public void processForest(int density, Point<Float> origin, int complexity, float size, int penWidth) {
+		float x = origin.x, y = origin.y;
+
+		for (int i = 0; i < density; i++) {
+			x += width/density;
+			if(Math.random() < 0.5f){
+				complexity--;
+			}
+			else{
+				complexity++;
+			}
+			
+			//TODO modifiy space, size, pen randomly
+			
+			toDraw.add(new Tree(new Point<Float>(x, y), complexity, size, penWidth));
+		}
+	}
+
 	public int getWidth() {
 		return width;
 	}
-	
+
 	public void setWidth(int width) {
 		this.width = width;
 	}
-	
+
 	public int getHeigth() {
 		return height;
 	}
-	
+
 	public void setHeigth(int heigth) {
 		this.height = heigth;
 	}
-	
+
 	public Color getBackground() {
 		return background;
 	}
 
 	public void setBackground(Color background) {
 		this.background = background;
-	}	
-	
-	public void draw(GdxGraphics g){
+	}
+
+	public void draw(GdxGraphics g) {
 		for (DrawableObject d : toDraw) {
 			d.draw(g);
 		}
 	}
-
-	//TODO calculer les décors
-	//Créer un vecteur avec tous les objets à donner au draw
-	//TODO gestion de la caméra
-	/*On peut connaitre la position de deux éléments qui se suivent, du coup
-	on peut calculer la distance qu'il y a entre les deux et ainsi déterminer la 
-	position et le champs de vision de la caméra.
-	Récupérer les positions dans une liste?*/
 }
