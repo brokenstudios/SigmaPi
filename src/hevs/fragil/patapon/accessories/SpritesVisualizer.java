d14a566249b47e85635f6043ef34daa036fe24f4
Loïc Gillioz <loic.gillioz@outlook.com>
2016-06-20 14:44:07 +0200

    Started new sprites visualizer

---
+++ src/hevs/fragil/patapon/accessories/SpritesVisualizer.java
@@ -0,0 +1,19 @@
+package hevs.fragil.patapon.accessories;
+
+import ch.hevs.gdx2d.desktop.PortableApplication;
+import ch.hevs.gdx2d.lib.GdxGraphics;
+
+public class SpritesVisualizer extends PortableApplication {
+
+	@Override
+	public void onInit() {
+		
+	}
+
+	@Override
+	public void onGraphicRender(GdxGraphics g) {
+		
+	}
+
+		
+}
